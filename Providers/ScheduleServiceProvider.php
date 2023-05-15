06119c519fcbc111086474cea2dca605c04a1145
imagina <msolano2505@gmail.com>
2023-05-15 16:19:09 -0500

    +Update: enable cron to sync the due invoices with bitrix

--- Providers/ScheduleServiceProvider.php
+++ Providers/ScheduleServiceProvider.php
@@ -14,12 +14,12 @@ class ScheduleServiceProvider extends ServiceProvider
       $schedule = $this->app->make(Schedule::class);
       //Sync first the clients
       $schedule->call(function () {
-        //\Modules\Iwhmcs\Jobs\SyncClientsToBitrix24::dispatch();
+        \Modules\Iwhmcs\Jobs\SyncClientsToBitrix24::dispatch();
         \Log::info("Iwhmcs::scheduled SyncClientsToBitrix24");
       })->timezone('America/Bogota')->dailyAt('07:00');
       //sync the due deals
       $schedule->call(function () {
-        //\Modules\Iwhmcs\Jobs\syncDueInvoicesItemsToBitrix::dispatch();
+        \Modules\Iwhmcs\Jobs\syncDueInvoicesItemsToBitrix::dispatch();
         \Log::info("Iwhmcs::scheduled syncDueInvoicesItemsToBitrix");
       })->timezone('America/Bogota')->dailyAt('08:00');
     });
