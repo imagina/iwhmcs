cef36a26cbb22ebbdf510af75487dcd847ecc876
msolanoimagina <msolano@contractor.renuityhome.com>
2023-10-17 09:22:04 -0500

    Disabled the syncs with WHCMS

--- Providers/ScheduleServiceProvider.php
+++ Providers/ScheduleServiceProvider.php
@@ -10,18 +10,18 @@ class ScheduleServiceProvider extends ServiceProvider
 {
   public function boot()
   {
-    $this->app->booted(function () {
-      $schedule = $this->app->make(Schedule::class);
-      //Sync first the clients
-      $schedule->call(function () {
-        \Modules\Iwhmcs\Jobs\SyncClientsToBitrix24::dispatch();
-        \Log::info("Iwhmcs::scheduled SyncClientsToBitrix24");
-      })->timezone('America/Bogota')->dailyAt('07:00');
-      //sync the due deals
-      $schedule->call(function () {
-        \Modules\Iwhmcs\Jobs\syncDueInvoicesItemsToBitrix::dispatch();
-        \Log::info("Iwhmcs::scheduled syncDueInvoicesItemsToBitrix");
-      })->timezone('America/Bogota')->dailyAt('08:00');
-    });
+//    $this->app->booted(function () {
+//      $schedule = $this->app->make(Schedule::class);
+//      //Sync first the clients
+//      $schedule->call(function () {
+//        \Modules\Iwhmcs\Jobs\SyncClientsToBitrix24::dispatch();
+//        \Log::info("Iwhmcs::scheduled SyncClientsToBitrix24");
+//      })->timezone('America/Bogota')->dailyAt('07:00');
+//      //sync the due deals
+//      $schedule->call(function () {
+//        \Modules\Iwhmcs\Jobs\syncDueInvoicesItemsToBitrix::dispatch();
+//        \Log::info("Iwhmcs::scheduled syncDueInvoicesItemsToBitrix");
+//      })->timezone('America/Bogota')->dailyAt('08:00');
+//    });
   }
 }
